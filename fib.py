34474d1272f43e7106f36036ee2c5c85e05c9097
ANKITPODDER0211 <72136836+ANKITPODDER0211@users.noreply.github.com>
2020-09-30 17:16:30 +0530

    Update fib.py
    Complexity is : O(n)

--- fib.py
+++ fib.py
@@ -1,12 +1,24 @@
-#fibonacci_approach
-#this is a recursive approach
-def fib(a):
+def fib1(a):
 	if a<=1:
 		return a
-	return fib(a-1) + fib(a-2)
+	return fib1(a-1) + fib1(a-2)
 
-#main code
+def fib2(l):
+	a = 0
+	b = 1
+	c = None
+	print("Fibonacci series is : 0 1 ",end="")
+	for i in range(l - 2):
+		c = a + b
+		a = b
+		b = c
+		print(b , end = " ")
+
 l = int(input("Enter th length of fibanacci series : "))
-print("Fib series is : ",end="")
-for i in range(l):
-	print(fib(i) , end=" ")
+ch = input("Enter 1 -> fib1() || Enter 2 -> fib2(l) : ")
+if ch=='1':
+	print("Fib series is : ",end="")
+	for i in range(l):
+		print(fib1(i) , end=" ")
+else:
+	fib2(l)
